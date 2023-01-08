715b51a0fd5d7395251f8122f4b341f4c12fd0d0
aniketgarje7 <garjeaniket7@gmail.com>
2023-01-08 16:52:08 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<App/>}/>
+        <Route path='/teamportal' element={<App/>}/>
+      
+    </Routes>
+    </Router>
   </React.StrictMode>
 );
 
